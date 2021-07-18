2e3434e9b2d642c71da1e3c748b98bede2133d0f
jonanOribe <jonan.oribe@gmail.com>
2021-07-18 18:23:19 +0200

    Basics

--- posts/views.py
+++ posts/views.py
@@ -1,36 +1,36 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 from datetime import datetime
 
 posts = [
     {
-        'name' : 'Mont Blac',
-        'user' : 'Yésica Cortés',
-        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' : 'https://picsum.photos/id/237/200/200'
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name' : 'Danilo',
-        'user' : 'Daniel Lopez',
-        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' : 'https://picsum.photos/id/237/200/200'
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name' : 'MVargas',
-        'user' : 'Manuel Vargas',
-        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' : 'https://picsum.photos/id/237/200/200'
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
 def list_posts(request):
-    content=[]
-    for post in posts:
-        for post in posts:
-            content.append("""
-                <p><strong>{name}</strong></p>
-                <p><small>{user} - <i>{timestamp}</i></small></p>
-                <figure><img src="{picture}"/></figure>
-                """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request,'feed.html', {'posts': posts})
