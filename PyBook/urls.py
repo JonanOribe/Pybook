69ba154a263b30a95079ac51aa29a7657f4d01c2
jonanOribe <jonan.oribe@gmail.com>
2021-07-21 21:11:11 +0200

    Update

--- PyBook/urls.py
+++ PyBook/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from PyBook import views as local_views
 from posts import views as post_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/',local_views.say_hi),
     
     path('posts/',post_views.list_posts)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
