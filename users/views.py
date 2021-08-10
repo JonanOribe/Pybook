0633e66508eb7b4bc685d92495bba605012b7323
jonanOribe <jonan.oribe@gmail.com>
2021-08-10 11:00:12 +0200

    Signup view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+from users.models import Profile
 
 def login_view(request):
     url='users/login.html'
@@ -20,3 +23,27 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def signup_view(request):
+        if request.method == 'POST':
+            username = request.POST['username']
+            passwd = request.POST['passwd']
+            passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
